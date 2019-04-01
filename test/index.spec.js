7436059f3977c9fb8361a5e9b07b2983d72d3a10
bkaptijn <Bas.Kaptijn@ictu.nl>
2019-04-01 10:36:02 +0200

    initialise project

---
+++ test/index.spec.js
@@ -0,0 +1,21 @@
+/* eslint-env mocha */
+import { expect } from 'chai'
+import * as cbd from '../src/index.js'
+//import { loadConnector } from '../src/connector-loader.js'
+
+//import sinon from 'sinon'
+//import { take, toArray } from 'rxjs/operators'
+
+describe('discipl-law-reg', () => {
+  describe('The discipl-law-reg library', () => {
+    it('should be able to do something', async () => {
+
+    })
+  },
+  ),
+  describe('The discipl-law-reg library with mocked connector', () => {
+    it('should be able to do something', async () => {
+
+    })
+  })
+})
