9203ebe11eac198b19d6c4b0f023d7c76879bc56
Ali Ozdemir <alozd@Alis-MBP.govroam.ictu.nl>
2019-05-27 14:29:15 +0200

    Added two tests with false results

--- test/index.spec.js
+++ test/index.spec.js
@@ -91,6 +91,48 @@ describe('discipl-law-reg', () => {
       })
     })
 
+    it('Does not find a match inside a multiple AND construction', async () => {
+      let parsedFact = lawReg.evaluateFactFunction('[fact1] EN [fact2]')
+      let core = lawReg.getAbundanceService().getCoreAPI()
+      let ssid = await core.newSsid('ephemeral')
+
+      const factResolver = (fact) => {
+        return fact === '[fact3]'
+      }
+      let result = await lawReg.checkExpression(parsedFact, ssid, { 'factResolver': factResolver })
+
+      expect(result).to.equal(false)
+
+      expect(parsedFact).to.deep.equal({
+        'expression': 'AND',
+        'operands': [
+          '[fact1]',
+          '[fact2]'
+        ]
+      })
+    })
+
+    it('Does not find a match inside a multiple OR construction', async () => {
+      let parsedFact = lawReg.evaluateFactFunction('[fact1] OF [fact2]')
+      let core = lawReg.getAbundanceService().getCoreAPI()
+      let ssid = await core.newSsid('ephemeral')
+
+      const factResolver = (fact) => {
+        return fact === '[fact3]'
+      }
+      let result = await lawReg.checkExpression(parsedFact, ssid, { 'factResolver': factResolver })
+
+      expect(result).to.equal(false)
+
+      expect(parsedFact).to.deep.equal({
+        'expression': 'OR',
+        'operands': [
+          '[fact1]',
+          '[fact2]'
+        ]
+      })
+    })
+
     it('correctly parses and solves an OR construction with a NOT and AND construction inside', async () => {
       let parsedFact = lawReg.evaluateFactFunction('(NIET [fact1]) OF ([fact2] EN [fact3])')
       let core = lawReg.getAbundanceService().getCoreAPI()
