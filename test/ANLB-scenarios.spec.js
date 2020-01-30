64ac455fc069dfd53560ee730c0bae7db353165a
Bill Kuipers <bikui@Air-van-Bill.el5.ictu.nl>
2020-01-30 15:34:00 +0100

    Changes in scenarios

--- test/ANLB-scenarios.spec.js
+++ test/ANLB-scenarios.spec.js
@@ -3,7 +3,7 @@ import Util from './../src/util'
 import ANLb from './ANLb.flint'
 import { LawReg } from '../src'
 import * as log from 'loglevel'
-log.getLogger('disciplLawReg').setLevel('warn')
+log.getLogger('disciplLawReg').setLevel('debug')
 const lawReg = new LawReg()
 
 const util = new Util(lawReg)
@@ -17,18 +17,27 @@ const factFunctionSpec = {
 
 const scenarios = [
   {
-    name: 'moet een collectief een aanvraag kunnen doen',
+    name: 'Aanvraag, verantwoorden, beoordelen',
     acts: [
       {
         act: '<<indienen betalingsaanvraag>>',
         actor: 'collectief'
-      }
+      },
+      {
+        act: '<<verantwoorden beheeractiviteiten en wijzigingen>>',
+        actor: 'collectief'
+      },
+      {
+        act: '<<Beoordelen verantwoording>>',
+        actor: 'RVO'
+      },
+
     ],
     facts: {
       '[identiteit van de begunstigde]': true,
       '[De naam van de regeling]': true,
       '[De totale omvang in hectares in 2 decimalen per leefgebied/deelgebied waarvoor betaling wordt gevraagd]': true,
-      '[De ligging (geometrie).]': [true, false],
+      '[De ligging (geometrie)]': [true, false, true, false],
       '[Het leefgebied waartoe het perceel behoort.]': true,
       '[Indien van toepassing het deelgebied waartoe het perceel behoort.]': true,
       '[Omvang van de beheerde oppervlakte (in hectares in 2 decimalen voor landbouwgrond, die exact past bij de geometrie (uit eerste punt).]': true,
@@ -39,7 +48,14 @@ const scenarios = [
       '[Aanvraagnummer gebiedsaanvraag (provincie) als bewijsstuk om te bepalen of de begunstigde voor betaling in aanmerking komt.]': true,
       '[De bewijsstukken die nodig zijn om te bepalen of de aanspraak op de steun/bijstand kan worden gemaakt. Hier moet worden gedacht aan stukken die de inhoud van de betalingsaanvraag onderbouwen]': true,
       '[betaalverzoek is tussen 1 maart en 15 mei ingediend]': true,
-      '[goedgekeurde gebiedsaanvraag]': true
+      '[goedgekeurde gebiedsaanvraag]': true,
+      '[Het deelgebied waartoe het perceel behoort (indien van toepassing).]': true,
+      '[De beheerfunctie waartoe het perceel behoort]': true,
+      '[Opgave van de uitgevoerde beheeractiviteit (Nederlandse versie van de koppeltabel) of combinatie van beheeractiviteiten.]': true,
+      '[kenmerk van de bijbehorende beschikking tot subsidieverlening van de gebiedsaanvraag]': true,
+      '[De wijzigingen (ten opzichte van jaarlijks beheer) die doorgevoerd zijn op de beheeractiviteiten met reden van wijzigen en per welke datum (indien van toepassing).]': true,
+      '[Digitale handtekening van de verantwoording]': true,
+      '[verantwoording]': true
     }
   }
 
